184954812d1c398dd3d48011a29bc2c01e54643e
Andrew London <104268984+Drew919@users.noreply.github.com>
2026-07-22 23:47:44 -0400

    Add files via upload
    Project class

---
+++ Project.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchonPM
+{
+    public class Project
+    {
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectOwner { get; set; }
+        public int ProjectID { get; set; }
+        public LinkedList<Staff> Stafflist { get; set; }
+        //Need to create staff object with staff name and ID
+        public LinkedList<Risk> Risklist { get; set; }
+        //Need to create Risk object with name, desc, catagory and risk priority
+        public LinkedList<Requirement> Requirmentlist { get; set; }
+        //Need to create Requirement object with name, desc, id, catagory (non/function),
+        public LinkedList<Deliverable> Deliverablelist { get; set; }
+        //Need to create Deliverable object with name, desc, id, due date, staff assignment
+
+
+    }
+}
