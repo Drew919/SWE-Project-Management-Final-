3078e9d9dd3b1e562907aadf495f73d9e9796ac5
klewi <178525879+klewIT@users.noreply.github.com>
2026-07-23 21:20:03 -0400

    Create Enums.cs
    Enums class implementation

---
+++ Main/Enums.cs
@@ -0,0 +1,18 @@
+﻿namespace DefaultNamespace;
+
+public class Enums
+{
+    public enum RiskPriority
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+
+    public enum ReqCategory
+    {
+        Functional,
+        NonFunctional
+    }
+}
