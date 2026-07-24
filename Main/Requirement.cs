96fc2bd401a952a17bf02cabbe685b17a14fd7a3
Corwin Melton <116982726+corwin-melton@users.noreply.github.com>
2026-07-24 05:35:18 -0400

    Removed the old console prototype and unused files

--- Main/Requirement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DefaultNamespace;
-
-public class Requirement
-{
-    public int Id {get; set;}
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public ReqCategory Category { get; set; }
-}
