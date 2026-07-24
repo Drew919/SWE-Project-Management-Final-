57d9a82f9b8ab6f6afe6b75c746c433c98c91ac3
klewi <178525879+klewIT@users.noreply.github.com>
2026-07-23 21:21:10 -0400

    Create Staff.cs
    Staff class implementation

---
+++ Main/Staff.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public class Staff
+{
+    public int ID { get; set; }
+    public string Name { get; set; } = string.Empty;
+    
+    public Staff(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+
+    public override string ToString() => $"[ID: {Id}, Name: {Name}]}";
+
+}
