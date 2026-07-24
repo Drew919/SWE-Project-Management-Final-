4ff2913e41821fcb116ee4f1964085f1e7447760
klewi <178525879+klewIT@users.noreply.github.com>
2026-07-23 21:20:53 -0400

    Create Risk.cs
    Risk class implementation

---
+++ Main/Risk.cs
@@ -0,0 +1,17 @@
+﻿namespace DefaultNamespace;
+
+public class Risk
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public RiskPriority Priority { get; set; }
+
+    public Risk(string n, string d, string c, string p)
+    {
+        Name = n;
+        Description = d;
+        Category = c;
+        Priority = p;
+    }
+}
