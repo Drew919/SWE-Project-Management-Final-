dab0daa8d57a00fd4e5dd32fb1d5a56b9d74dd5f
klewi <178525879+klewIT@users.noreply.github.com>
2026-07-23 21:19:44 -0400

    Create Deliverable.cs
    Deliverable class implementation

---
+++ Main/Deliverable.cs
@@ -0,0 +1,20 @@
+﻿namespace DefaultNamespace;
+
+public class Deliverable
+{
+    public int ID { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime DueDate { get; set; }
+    public Staff? AssignedStaff { get; set; }
+
+    public Deliverable(int id, string n, string d, DateTime dd, Staff? astaff = null)
+    
+    {
+        Id  = id;
+        Name = n;
+        Description = d;
+        DueDate = dd;
+        AssignedStaff = astaff;
+    }
+}
