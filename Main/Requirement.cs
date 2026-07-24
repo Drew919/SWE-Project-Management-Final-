8e099da7cb192da48839cd5b24ed5a2b1f772412
klewi <178525879+klewIT@users.noreply.github.com>
2026-07-23 21:20:34 -0400

    Create Requirement.cs
    Requirement class implementation

---
+++ Main/Requirement.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultNamespace;
+
+public class Requirement
+{
+    public int Id {get; set;}
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ReqCategory Category { get; set; }
+}
